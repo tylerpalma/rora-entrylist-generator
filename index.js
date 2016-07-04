5ee7846116d5251c8db2293d437afb1f53499000
Tyler Palma <tylerpalma@gmail.com>
2016-07-03 18:51:28 -0700

    prompt for setting options

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var Spreadsheet = require('google-spreadsheets');
 var _ = require('lodash');
 var async = require('async');
+var prompt = require('prompt');
 
 
 var opt = {
@@ -12,83 +13,110 @@ var opt = {
   defaultSkin: '00_official',
 };
 
-async.waterfall([
-  function(callback) {
-    Spreadsheet({
-      key: opt.masterKey,
-    }, function(err, spreadsheet) {
-      spreadsheet.worksheets[0].cells({
-        range: 'R2C1:R100C8'
-      }, function(err, driverCells) {
-        callback(null, driverCells);
-      });
-    });
+prompt.start();
+
+prompt.get([{
+    name: 'region',
+    description: 'Which region?',
+    type: 'string',
+    default: 'NA'
   },
-  function(driverCells, callback) {
-    Spreadsheet({
-      key: opt.liveryKey,
-    }, function(err, spreadsheet) {
-      spreadsheet.worksheets[0].cells({
-        range: 'R3C1:R100C2'
-      }, function(err, liveryCells) {
-        callback(null, driverCells, liveryCells)
-      });
-    });
+  {
+    name: 'defaultCar',
+    description: 'What is the default car?',
+    default: 'ks_mazda_mx5_cup',
   },
-  function(driverCells, liveryCells, callback) {
-    var liveries = _.map(liveryCells.cells, function(row) {
-      return {
-        redditName: _.find(row, { 'col': '1' }).value,
-        liveryDirectory: _.find(row, {'col': '2'}).value,
-      };
-    });
+  {
+    name: 'defaultSkin',
+    description: 'What will be the default skin?',
+    default: '00_official',
+  }], function (err, result) {
+    opt.region = result.region;
+    opt.carModel = result.defaultCar;
+    opt.defaultSkin = result.defaultSkin;
 
-    var data = _.map(driverCells.cells, function(row) {
-      var driverSkin = _.find(liveries, { 'redditName': _.find(row, { 'col': '1' }).value });
-      //set default skins
-      if(driverSkin == undefined) {
-        driverSkin = opt.defaultSkin;
-      } else {
-        driverSkin = driverSkin.liveryDirectory;
-      }
+    generateList();
+});
 
-      return {
-        redditName: _.find(row, { 'col': '1' }).value,
-        acName: _.find(row, { 'col': '2' }).value,
-        guid: _.find(row, { 'col': '3' }).value,
-        division: _.find(row, { 'col': '4' }).value,
-        region: _.find(row, { 'col': '5' }).value,
-        team: (_.find(row, { 'col': '6' }) ? _.find(row, { 'col': '6' }).value : '') ,
-        driverNumber: _.find(row, { 'col': '8' }).value,
-        skin: driverSkin,
-      };
-    });
+function generateList() {
+  async.waterfall([
+    function(callback) {
+      Spreadsheet({
+        key: opt.masterKey,
+      }, function(err, spreadsheet) {
+        spreadsheet.worksheets[0].cells({
+          range: 'R2C1:R100C8'
+        }, function(err, driverCells) {
+          callback(null, driverCells);
+        });
+      });
+    },
+    function(driverCells, callback) {
+      Spreadsheet({
+        key: opt.liveryKey,
+      }, function(err, spreadsheet) {
+        spreadsheet.worksheets[0].cells({
+          range: 'R3C1:R100C2'
+        }, function(err, liveryCells) {
+          callback(null, driverCells, liveryCells)
+        });
+      });
+    },
+    function(driverCells, liveryCells, callback) {
+      var liveries = _.map(liveryCells.cells, function(row) {
+        return {
+          redditName: _.find(row, { 'col': '1' }).value,
+          liveryDirectory: _.find(row, {'col': '2'}).value,
+        };
+      });
 
-    callback(null, data);
-  }
-], function(err, result) {
-    var entrylist = '';
-    var driverCount = 0;
+      var data = _.map(driverCells.cells, function(row) {
+        var driverSkin = _.find(liveries, { 'redditName': _.find(row, { 'col': '1' }).value });
+        //set default skins
+        if(driverSkin == undefined) {
+          driverSkin = opt.defaultSkin;
+        } else {
+          driverSkin = driverSkin.liveryDirectory;
+        }
 
-    _.forEach(result, function(driver) {
-      if(driver.region == opt.region) {
-        var string = '[CAR_'+ driverCount +']\n';
-        string += 'DRIVERNAME=' + driver.acName + '\n';
-        string += 'TEAM=\n';
-        string += 'MODEL=' + opt.carModel + '\n';
-        string += 'SKIN='+ driver.skin +'\n';
-        string += 'GUID=' + driver.guid + '\n';
-        string += 'SPECTATOR_MODE=0\n';
-        string += 'BALLAST=0\n';
-        string += ' \n';
+        return {
+          redditName: _.find(row, { 'col': '1' }).value,
+          acName: _.find(row, { 'col': '2' }).value,
+          guid: _.find(row, { 'col': '3' }).value,
+          division: _.find(row, { 'col': '4' }).value,
+          region: _.find(row, { 'col': '5' }).value,
+          team: (_.find(row, { 'col': '6' }) ? _.find(row, { 'col': '6' }).value : '') ,
+          driverNumber: _.find(row, { 'col': '8' }).value,
+          skin: driverSkin,
+        };
+      });
 
-        driverCount++;
+      callback(null, data);
+    }
+  ], function(err, result) {
+      var entrylist = '';
+      var driverCount = 0;
 
-        entrylist += string;
-      }
-    });
-    fs.writeFile('entry_list.ini', entrylist, function(err) {
-      console.log('Entry_list.ini has been generated successfully.');
-    });
-  }
-);
+      _.forEach(result, function(driver) {
+        if(driver.region == opt.region) {
+          var string = '[CAR_'+ driverCount +']\n';
+          string += 'DRIVERNAME=' + driver.acName + '\n';
+          string += 'TEAM=\n';
+          string += 'MODEL=' + opt.carModel + '\n';
+          string += 'SKIN='+ driver.skin +'\n';
+          string += 'GUID=' + driver.guid + '\n';
+          string += 'SPECTATOR_MODE=0\n';
+          string += 'BALLAST=0\n';
+          string += ' \n';
+
+          driverCount++;
+
+          entrylist += string;
+        }
+      });
+      fs.writeFile('entry_list.ini', entrylist, function(err) {
+        console.log('Entry_list.ini has been generated successfully.');
+      });
+    }
+  );
+}
